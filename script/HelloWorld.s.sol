9e094320c35beeadbcf939cfd14d66cfb0b96cdb
Alexander Camuto <45801863+alexander-camuto@users.noreply.github.com>
2023-04-26 09:06:57 +0100

    feat: helloworld

--- script/HelloWorld.s.sol
+++ script/HelloWorld.s.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/HelloWorld.sol";
 
-contract CounterScript is Script {
+contract HelloWorldScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        HelloWorld test = new HelloWorld();
     }
 }
